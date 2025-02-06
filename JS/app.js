b415bc65062857eef017f7607174b26131a7238a
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-02-06 13:14:51 +0530

    Started a Javascript

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+const userLocation = document.getElementById("userLocation"),
+  converter = document.getElementById("converter"),
+  weatherIcon = document.querySelector(".weatherIcon"),
+  temperature = document.querySelector(".tempreture"),
+  feelsLike = document.querySelector(".feelsLike"),
+  description = document.querySelector(".description"),
+  date = document.querySelector(".date"),
+  city = document.querySelector(".city"),
+  hValue = document.getElementById("hValue"),
+  wValue = document.getElementById("wValue"),
+  srValue = document.getElementById("srValue"),
+  ssValue = document.getElementById("ssValue"),
+  cValue = document.getElementById("cValue"),
+uvValue = document.getElementById("uvValue"),
+pValue = document.getElementById("pValue"),
+forecast = document.querySelector(".forcast")
+
+
+
+
+
